4b152e10f119712dbcd1de542348d73819fa91b9
JeremyLetang <letang.jeremy@gmail.com>
2014-01-22 11:33:13 -0500

    add Problem 0: joiner.rs

---
+++ joiner.rs
@@ -0,0 +1,16 @@
+// Problem 0: join two files
+
+use std::os;
+use std::io::File;
+
+fn main() {
+	let args = os::args();
+	if args.len() != 3 { fail!("Need two files to join !"); }
+	
+	let share1 = File::open(&Path::new(args[1].clone())).read_to_end();
+	let share2 = File::open(&Path::new(args[2].clone())).read_to_end();
+	for i in range(0, share1.len()) {
+		print!("{}", (share1[i] ^ share2[i]) as char)
+	}
+	println!("");
+}
